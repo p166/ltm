fe0f26a4ded41a4587a22f4a3e1b248da6d79fa8
p166 <dsimakov80@gmail.com>
2026-05-25 16:26:17 +0400

    x

--- ltm.js
+++ ltm.js
@@ -1,5 +1,5 @@
 (function () {
     'use strict';
-    Lampa.Utils.putScriptAsync(['https://cdn.jsdelivr.net/gh/p166/ltm@1b885209053e64d365544e94c3d0681269a8eca1/torrentmanager.js'], function () { });
+    Lampa.Utils.putScriptAsync(['https://cdn.jsdelivr.net/gh/p166/ltm@c65f8f63de3b2909daaf4b9aa47571a5bab5fc9f/torrentmanager.js'], function () { });
 
 })();
