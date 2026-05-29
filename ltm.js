4622506c0027a091a7998b982bb2484318933d18
p166 <dsimakov80@gmail.com>
2026-05-29 14:05:51 +0400

    fix: update pinned torrentmanager commit hash

--- ltm.js
+++ ltm.js
@@ -1,5 +1,5 @@
 (function () {
     'use strict';
-    Lampa.Utils.putScriptAsync(['https://cdn.jsdelivr.net/gh/p166/ltm@c65f8f63de3b2909daaf4b9aa47571a5bab5fc9f/torrentmanager.js'], function () { });
+    Lampa.Utils.putScriptAsync(['https://cdn.jsdelivr.net/gh/p166/ltm@a654ed03fe1162386d80a16fce255f4e32aeb064/torrentmanager.js'], function () { });
 
 })();
